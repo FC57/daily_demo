aac04980f32b27cdc7a9c077ca14952ad14a1473
FC57 <franky_57@163.com>
2024-09-19 02:29:03 +0800

    feat(react): ✨ add react preview and component of IconButton

---
+++ react/component/IconButton/index.jsx
@@ -0,0 +1,25 @@
+// 实际开发中 import { Button } from 'antd';
+const { Button } = antd;
+
+/**
+ * 自定义图标按钮组件
+ * * iconName 为 assets/imgs/btns 图片名称（如：new.png 和 new.gray.png 为一组的图片，取 new）
+ */
+function IconBtn({ children, iconName, disabled = false, ...resetOption }) {
+  let cIcon = null;
+  if (typeof iconName === 'string') {
+    // 图片路径
+    // webpack 中使用 require(`/assets/imgs/btns/${iconName}${disabled ? '.gray' : ''}.png`)
+
+    // vite 中使用 const imgs = import.meta.glob('/assets/imgs/btns/*.png') 获取所有图片
+    // const iconPath = imgs[`/assets/imgs/btns/${iconName}${disabled ? '.gray' : ''}.png`]
+
+    const iconPath = `/assets/imgs/btns/${iconName}${disabled ? '.gray' : ''}.png`;
+    cIcon = <img src={iconPath} alt={iconName} />;
+  }
+  return (
+    <Button className="icon-btn" {...resetOption} icon={cIcon} disabled={disabled}>
+      {children}
+    </Button>
+  );
+}
